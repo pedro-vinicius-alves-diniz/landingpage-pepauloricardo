9b872e680ece5f4b33138079956d0b273b2f8057
Pedro Vinicius Alves Diniz <pedrinhovini318@icloud.com>
2024-08-07 23:00:23 +0200

    projeto finalizado

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    autoplay: true,
+    delay:1000,
+    breakpoints: {
+        1041: {
+            slidesPerView:3,
+            spaceBetween: 30
+        },
+        769: {
+            slidesPerView:2,
+            spaceBetween: 30
+        }
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    mousewheel: true,
+    keyboard: true,
+});
